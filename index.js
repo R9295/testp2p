cd99fce9254d2bed2aac819ce76d4df2e0c0cdf8
aarnav <aarnavbos@gmail.com>
2020-03-06 17:52:00 +0100

    asd

--- index.js
+++ index.js
@@ -4,23 +4,26 @@ const MulticastDNS = require("libp2p-mdns");
 const TCP = require("libp2p-tcp");
 const websocket = require("libp2p-websockets");
 
-(async () => {
-  const node = await libp2p.create({
-    modules: {
-      transport: [TCP],
-  //    streamMuxer: [Mplex],
-  //    connEncryption: [SECIO],
-      peerDiscovery: [MulticastDNS]
-    },
-    config: {
-      peerDiscovery: {
-        mdns: {
-          interval: 20e3,
-          enabled: true
-        }
+libp2p.create({
+  modules: {
+    transport: [TCP],
+    //    streamMuxer: [Mplex],
+    //    connEncryption: [SECIO],
+    peerDiscovery: [MulticastDNS]
+  },
+  config: {
+    peerDiscovery: {
+      mdns: {
+        interval: 2,
+        broadcast: true,
+        enabled: true
       }
     }
-  });
-  node.on('peer:discovery', (peer) => console.log('Discovered:', peer.id.toB58String()))
-  await node.start()
-})();
+  }
+}).then(() => {
+  node.on("peer:discovery", peer =>
+    console.log("Discovered:", peer.id.toB58String())
+  );
+  await node.start();
+  
+})
