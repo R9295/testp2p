8a4b4889ada9f6872e5335becc2f5be571c66510
aarnav <aarnavbos@gmail.com>
2020-03-09 13:45:23 +0100

    asd

--- index.js
+++ index.js
@@ -3,27 +3,35 @@ const kad = require("libp2p-kad-dht");
 const MulticastDNS = require("libp2p-mdns");
 const TCP = require("libp2p-tcp");
 const websocket = require("libp2p-websockets");
+const PeerInfo = require("peer-info");
 
-libp2p.create({
-  modules: {
-    transport: [TCP],
-    //    streamMuxer: [Mplex],
-    //    connEncryption: [SECIO],
-    peerDiscovery: [MulticastDNS]
-  },
-  config: {
-    peerDiscovery: {
-      mdns: {
-        interval: 2,
-        broadcast: true,
-        enabled: true
+PeerInfo.create().then(p => {
+  p.multiaddrs.add(multiaddr("/ip4/127.0.0.1/tcp/20001"));
+  libp2p
+    .create({
+      modules: {
+        transport: [TCP],
+        //    streamMuxer: [Mplex],
+        //    connEncryption: [SECIO],
+        peerDiscovery: [MulticastDNS]
+      },
+      config: {
+        peerDiscovery: {
+          autodial: true,
+          [MulticastDNS.tag]: {
+            broadcast: true,
+            interval: 1000,
+            enabled: true,
+            
+            port: 5003
+          }
+        }
       }
-    }
-  }
-}).then(() => {
-  node.on("peer:discovery", peer =>
-    console.log("Discovered:", peer.id.toB58String())
-  );
-  await node.start();
-  
-})
+    })
+    .then(async () => {
+      node.on("peer:discovery", peer =>
+        console.log("Discovered:", peer.id.toB58String())
+      );
+      await node.start();
+    });
+});
