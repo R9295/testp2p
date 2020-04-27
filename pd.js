d2a0cd733401125c5cbf74488ae38daca1c74b03
aarnav <aarnavbos@gmail.com>
2020-04-27 15:38:01 +0200

    add eg

---
+++ pd.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+"use strict";
+
+const Libp2p = require("libp2p");
+const TCP = require("libp2p-tcp");
+const Mplex = require("libp2p-mplex");
+const SECIO = require("libp2p-secio");
+const MulticastDNS = require("libp2p-mdns");
+
+const createNode = async () => {
+  const node = await Libp2p.create({
+    modules: {
+      transport: [TCP],
+      streamMuxer: [Mplex],
+      connEncryption: [SECIO],
+      peerDiscovery: [MulticastDNS]
+    },
+    config: {
+      peerDiscovery: {
+        mdns: {
+          interval: 20e3,
+          enabled: true
+        }
+      }
+    }
+  });
+  node.peerInfo.multiaddrs.add("/ip4/0.0.0.0/tcp/0");
+
+  return node;
+};
+
+(async () => {
+  const node1 = await createNode();
+
+  node1.on("peer:discovery", peer =>
+    console.log("Discovered:", peer.id.toB58String())
+  );
+  await node1.start();
+  node1.peerInfo.multiaddrs.forEach(ma => console.log(ma.toString()));
+})();
